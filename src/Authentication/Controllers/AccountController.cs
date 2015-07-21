534e6aeb9bf1de2be09dcace9b7a690a874c3e48
Dominick Baier <dbaier@leastprivilege.com>
2015-07-21 15:04:32 +0200

    initial commit

---
+++ src/Authentication/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using Authentication.Models;
+using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Authentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/Login
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (ModelState.IsValid)
+            {
+                if (model.UserName == model.Password)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim("sub", model.UserName),
+                        new Claim("name", "bob"),
+                        new Claim("email", "bob@smith.com")
+                    };
+
+                    var id = new ClaimsIdentity(claims, "local", "name", "role");
+                    Context.Authentication.SignIn("Cookies", new ClaimsPrincipal(id));
+                    
+                    return RedirectToLocal(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Logoff()
+        {
+            Context.Authentication.SignOut();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+    }
+}
---
+++ src/Authentication/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.Runtime;
+
+namespace Authentication
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.MinimumLevel = LogLevel.Information;
+            loggerFactory.AddConsole();
+
+            app.UseCookieAuthentication(options =>
+            {
+                options.LoginPath = "/account/login";
+
+                options.AuthenticationScheme = "Cookies";
+                options.AutomaticAuthentication = true;
+            });
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
