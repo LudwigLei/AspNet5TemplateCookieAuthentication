7517a1703c3c4eda9d868bf824e24312f8686e28
Dominick Baier <dbaier@leastprivilege.com>
2015-07-24 08:51:12 +0200

    simplified HTML

--- src/Authentication/Controllers/AccountController.cs
+++ src/Authentication/Controllers/AccountController.cs
@@ -7,49 +7,34 @@ namespace Authentication.Controllers
 {
     public class AccountController : Controller
     {
-        //
-        // GET: /Account/Login
         [HttpGet]
-        [AllowAnonymous]
         public IActionResult Login(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
 
-        //
-        // POST: /Account/Login
         [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public IActionResult Login(LoginViewModel model, string returnUrl = null)
+        public IActionResult Login(string userName, string password, string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
 
-            if (ModelState.IsValid)
+            if (userName == password)
             {
-                if (model.UserName == model.Password)
-                {
-                    var claims = new List<Claim>
+                var claims = new List<Claim>
                     {
-                        new Claim("sub", model.UserName),
+                        new Claim("sub", userName),
                         new Claim("name", "bob"),
                         new Claim("email", "bob@smith.com")
                     };
 
-                    var id = new ClaimsIdentity(claims, "local", "name", "role");
-                    Context.Authentication.SignIn("Cookies", new ClaimsPrincipal(id));
-                    
-                    return RedirectToLocal(returnUrl);
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return View(model);
-                }
+                var id = new ClaimsIdentity(claims, "local", "name", "role");
+                Context.Authentication.SignIn("Cookies", new ClaimsPrincipal(id));
+
+                return RedirectToLocal(returnUrl);
             }
 
-            return View(model);
+            return View();
         }
 
         public IActionResult Logoff()
